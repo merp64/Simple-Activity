9e3e425efae6b8725cccfde26a1751f820ee138b
merp64 <82673037+merp64@users.noreply.github.com>
2022-05-15 14:54:35 -0400

    Simple Activity

---
+++ app/src/main/java/com/example/simpleactivityapp/MainActivity2.kt
@@ -0,0 +1,16 @@
+package com.example.simpleactivityapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        val actionBar = supportActionBar
+
+        actionBar !!.title = "Second Activity"
+        actionBar.setDisplayHomeAsUpEnabled(true)
+
+    }
+}
